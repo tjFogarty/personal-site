b4b0089034259a7788f7fca5406ce0c41dc6875e
T.J. Fogarty <fogarty.tj@gmail.com>
2017-11-30 21:30:25 +0000

    Sets up some pagination

--- config/general.php
+++ config/general.php
@@ -23,6 +23,8 @@ return [
 
         // The secure key Craft will use for hashing and encrypting data
         'securityKey' => getenv('SECURITY_KEY'),
+        
+        'pageTrigger' => 'page/',
     ],
 
     // Dev environment settings
