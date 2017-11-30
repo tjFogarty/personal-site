b5d2927f2cbca4f98784d96e48af0bfca4147890
T.J. Fogarty <fogarty.tj@gmail.com>
2017-11-30 22:08:20 +0000

    Adds mix plugin

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log('what')
+import { ready } from './utils'
+
+ready(() => {
+  // let pre = document.querySelector('pre')
+  
+  // if (pre) {
+  //   const Prism = System.import('prismjs').then(Prism => {
+  //     Prism.highlightAll()
+  //     console.log(Prism)
+  //   })
+  // }
+})
---
+++ src/js/utils.js
@@ -0,0 +1,11 @@
+export function ready(fn) {
+  if (
+    document.attachEvent
+      ? document.readyState === 'complete'
+      : document.readyState !== 'loading'
+  ) {
+    fn()
+  } else {
+    document.addEventListener('DOMContentLoaded', fn)
+  }
+}
