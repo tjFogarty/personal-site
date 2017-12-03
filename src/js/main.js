1fea48c39b67abf91978c853fc5467e453dfef69
T.J. Fogarty <fogarty.tj@gmail.com>
2017-12-03 11:27:40 +0000

    Updates chunk file handling

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,12 @@
 import { ready } from './utils'
 
 ready(() => {
+  let pre = document.querySelector('pre')
   
+  if (pre) {
+    System.import(/* webpackChunkName: "prism" */'prismjs')
+      .then(Prism => {
+        Prism.highlightAll()
+      })
+  }
 })
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,8 +2,15 @@ const mix = require('laravel-mix')
 const tailwindcss = require('tailwindcss')
 
 mix
+  .setPublicPath(__dirname)
   .js('src/js/main.js', 'web/assets/js')
   .less('src/less/app.less', 'web/assets/css')
+  .webpackConfig({
+    output: {
+      chunkFilename: 'web/assets/js/chunks/[name].js'
+    }
+  })
   .options({
     postCss: [tailwindcss('./tailwind.js')]
   })
+  .version()
