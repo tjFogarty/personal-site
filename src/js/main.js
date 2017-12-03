957299f7412c228afa7a76e6131904a6c4562eab
T.J. Fogarty <fogarty.tj@gmail.com>
2017-12-03 11:32:23 +0000

    Remove prism

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,5 @@
 import { ready } from './utils'
 
 ready(() => {
-  let pre = document.querySelector('pre')
-  
-  if (pre) {
-    System.import(/* webpackChunkName: "prism" */'prismjs')
-      .then(Prism => {
-        Prism.highlightAll()
-      })
-  }
+
 })
--- webpack.mix.js
+++ webpack.mix.js
@@ -5,11 +5,6 @@ mix
   .setPublicPath(__dirname)
   .js('src/js/main.js', 'web/assets/js')
   .less('src/less/app.less', 'web/assets/css')
-  .webpackConfig({
-    output: {
-      chunkFilename: 'web/assets/js/chunks/[name].js'
-    }
-  })
   .options({
     postCss: [tailwindcss('./tailwind.js')]
   })
