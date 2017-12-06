cfdd16698751eb2c35c4de10218d380d29e96d47
T.J. Fogarty <fogarty.tj@gmail.com>
2017-12-06 21:37:56 +0000

    Uses purge css to cut down on css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,19 @@
+// Based on https://medium.com/@AndrewDelPrete/using-purifycss-to-remove-unused-tailwind-css-classes-173b3ee8ee01
+
 const mix = require('laravel-mix')
 const tailwindcss = require('tailwindcss')
+const glob = require('glob-all')
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+
+// Custom PurgeCSS extractor for Tailwind that allows special characters in
+// class names.
+//
+// https://github.com/FullHuman/purgecss#extractor
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g)
+  }
+}
 
 mix
   .setPublicPath(__dirname)
@@ -11,3 +25,28 @@ mix
     postCss: [tailwindcss('./tailwind.js')]
   })
   .version()
+
+// Only run PurgeCSS during production builds for faster development builds
+// and so you still have the full set of utilities available during
+// development.
+if (mix.inProduction()) {
+  mix.webpackConfig({
+    plugins: [
+      new PurgecssPlugin({
+        // Specify the locations of any files you want to scan for class names.
+        paths: glob.sync([
+          path.join(__dirname, 'templates/**/*.twig')
+        ]),
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+
+            // Specify the file extensions to include when scanning for
+            // class names.
+            extensions: ['html', 'js', 'php', 'twig']
+          }
+        ]
+      })
+    ]
+  })
+}
