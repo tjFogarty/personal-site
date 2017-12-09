5e2db1fe11b4d6ed053a2db92a1efab186869902
T.J. Fogarty <fogarty.tj@gmail.com>
2017-12-09 23:14:06 +0000

    Adds mobile typography

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -2,6 +2,13 @@ import { ready } from './utils'
 import { TitleEffect } from './title-effect'
 
 ready(async () => {
+  let toggleNav = document.querySelector('.js-toggle-nav')
+  let navInner = document.querySelector('.c-nav__inner')
+
+  toggleNav.addEventListener('click', () => {
+    navInner.classList.toggle('is-open')
+  })
+
   TitleEffect.init()
 
   if (document.querySelector('pre')) {
