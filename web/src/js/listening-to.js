6e126de3cb60e008d6cd5612f1a2ef5fa16bae5d
T.J. Fogarty <fogarty.tj@gmail.com>
2017-12-10 12:37:33 +0000

    Adds styling for now playing section

--- web/src/js/listening-to.js
+++ web/src/js/listening-to.js
@@ -5,25 +5,33 @@ const endpoint =
 
 export const ListeningTo = {
   container: document.querySelector('.js-lt-container'),
+  trigger: document.querySelector('.js-lt-trigger'),
+  nowPlayingEl: document.querySelector('.c-now-playing'),
 
   async init() {
     try {
       let { recenttracks } = await fetch(endpoint).then(res => res.json())
       this.updateRecentTrackDisplay(recenttracks.track[0])
+
+      this.trigger.addEventListener('click', () => {
+        this.nowPlayingEl.classList.toggle('is-active')
+      })
     } catch (e) {}
   },
 
   updateRecentTrackDisplay({ album, artist, image, url }) {
     let html = `
-      <div class="flex items-center mr-5">
-        <i data-feather="music" class="mr-4"></i>
-        <a href="${
-          url
-        }" rel="noopener" target="_blank" class="flex items-center">
-          <img class="mr-3" src="${image[0]['#text']}">
+      <div class="c-now-playing__info" style="background-image: url('${
+        image[2]['#text']
+      }'); background-size: cover; background-position: 50%;">
+        <div class="c-now-playing__text">
+          <a href="${
+            url
+          }" rel="noopener" target="_blank" class="flex items-center">
           <span class="text-sm">${album['#text']} -&nbsp;</span>
           <span class="text-sm">${artist['#text']}</span>
-        </a>
+          </a>
+        </div>
       </div>
     `
 
