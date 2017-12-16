d1efd72e0bd6cde2080e2c8db4bd5bc54905ba04
T.J. Fogarty <fogarty.tj@gmail.com>
2017-12-16 14:35:06 +0000

    Simplifies now playing feature

--- web/src/js/listening-to.js
+++ web/src/js/listening-to.js
@@ -2,29 +2,19 @@ const endpoint =
   'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=cowpuncher&api_key=dc2e7242feba68442dda3281bdd202f0&format=json&limit=1'
 
 export const ListeningTo = {
-  container: document.querySelector('.js-lt-container'),
-  trigger: document.querySelector('.js-lt-trigger'),
-  nowPlayingEl: document.querySelector('.c-now-playing'),
-  urlEl: document.querySelector('.js-lt-url'),
-  artistEl: document.querySelector('.js-lt-artist'),
-  albumEl: document.querySelector('.js-lt-album'),
-  infoEl: document.querySelector('.js-lt-info'),
-
   async init() {
     try {
       let { recenttracks } = await fetch(endpoint).then(res => res.json())
-      this.updateRecentTrackDisplay(recenttracks.track[0])
-
-      this.trigger.addEventListener('click', () => {
-        this.nowPlayingEl.classList.toggle('is-active')
-      })
-    } catch (e) {}
+      this.updateRecentTrackVariable(recenttracks.track[0])
+    } catch (e) {
+      console.log(e)
+    }
   },
 
-  updateRecentTrackDisplay({ album, artist, image, url }) {
-    this.albumEl.textContent = album['#text']
-    this.artistEl.textContent = artist['#text']
-    this.urlEl.setAttribute('href', url)
-    this.infoEl.style.backgroundImage = `url("${image[2]['#text']}")`
+  updateRecentTrackVariable({ artist, name }) {
+    document.documentElement.style.setProperty(
+      '--current-track',
+      `'Currently listening to:  ${name} by ${artist['#text']}'`
+    )
   }
 }
--- web/src/js/main.js
+++ web/src/js/main.js
@@ -1,19 +1,21 @@
 import { ready } from './utils'
-import { TitleEffect } from './title-effect'
 import { mobileNav } from './mobile-nav'
 import { ListeningTo } from './listening-to'
 import feather from 'feather-icons'
 
 ready(async () => {
   mobileNav()
-  TitleEffect.init()
   feather.replace()
   ListeningTo.init()
 
   if (document.querySelector('pre')) {
-    let hljs = await System.import(
-      /* webpackChunkName: "hljs" */ 'highlight.js'
-    )
-    hljs.initHighlightingOnLoad()
+    try {
+      let hljs = await System.import(
+        /* webpackChunkName: "hljs" */ 'highlight.js'
+      )
+      hljs.initHighlightingOnLoad()
+    } catch (e) {
+      console.log('Error loading highlight.js', e)
+    }
   }
 })
