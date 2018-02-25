bb8bf1ab19e9e11909a832cac236379dd274649b
T.J. Fogarty <fogarty.tj@gmail.com>
2018-02-25 19:28:00 +0000

    Adds JSON Client for lastfm requests

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -1,13 +1,11 @@
 import { ready, showDeveloperMessage } from './utils'
 import { mobileNav } from './mobile-nav'
-import { ListeningTo } from './listening-to'
 import { Search } from './search'
 import { PageVisibility } from './page-visibility'
 import { Intro } from './intro'
 
 ready(async () => {
   mobileNav()
-  ListeningTo.init()
   Search.init()
   PageVisibility.init()
   Intro.init()
