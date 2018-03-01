61c629dc3317792eed3924a76cec5871a07f933c
T.J. Fogarty <fogarty.tj@gmail.com>
2018-03-01 11:50:11 +0000

    Adds some parallax to the hero image

--- web/src/js/intro.js
+++ web/src/js/intro.js
@@ -1,5 +1,23 @@
-export const Intro = {
-  init() {
+import { create } from 'basicscroll'
 
+export const Intro = {
+  hero: document.querySelector('.c-hero'),
+
+  init() {
+    if (this.hero) {
+      const instance = create({
+        elem: this.hero,
+        from: '0',
+        to: '100%',
+        props: {
+          '--hero-position-y': {
+            from: '0%',
+            to: '5%'
+          }
+        }
+      })
+
+      instance.start()
+    }
   }
 }
