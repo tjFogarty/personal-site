138d0f788c330d752ee29a6fda399cbd81410317
T.J. Fogarty <fogarty.tj@gmail.com>
2018-03-13 22:15:10 +0000

    Updates Craft to RC15

--- config/general.php
+++ config/general.php
@@ -30,7 +30,7 @@ return [
     // Dev environment settings
     'dev' => [
         // Base site URL
-        'siteUrl' => 'http://tj-craft.dev',
+        'siteUrl' => 'https://tj-craft.dev',
 
         // Dev Mode (see https://craftcms.com/support/dev-mode)
         'devMode' => true,
